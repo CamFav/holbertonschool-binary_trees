c3ca9b7229ccbb8685124af287d3eaef23606ce5
Meashyshy <camillefvrl@gmail.com>
2023-05-15 03:01:53 -0700

    T1 - Completed (Insert node as left-child of another node)

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,10 +11,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *NewNode = NULL;
 
-    if (!parent)
+	if (!parent)
 		return (NULL);
 
-    NewNode = binary_tree_node(parent, value);
+	NewNode = binary_tree_node(parent, value);
 
 	if (!NewNode)
 	{
