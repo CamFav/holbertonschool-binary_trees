4a1224e69a3df6434db29f2c06cd59f1b27bb9ef
Meashyshy <camillefvrl@gmail.com>
2023-05-15 05:10:24 -0700

    Betty Coding Style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,5 +9,9 @@
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+	if (!tree)
+		return;
+	binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->right, func);
+	func(tree->n);
 }
