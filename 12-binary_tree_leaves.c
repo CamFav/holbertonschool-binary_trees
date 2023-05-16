271ef7eb2047e85bf4af4d3803ccaeebd447bbba
Meashyshy <camillefvrl@gmail.com>
2023-05-16 02:16:25 -0700

    Betty Coding Style

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,9 +12,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
+
 	leaves += binary_tree_leaves(tree->left);
 	leaves += binary_tree_leaves(tree->right);
+
 	return (leaves);
 }
