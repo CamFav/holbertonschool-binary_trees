f65590d2566d25a5bfde718f6f7afeac8e473ac9
Meashyshy <camillefvrl@gmail.com>
2023-05-16 06:09:24 -0700

    Betty coding style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,9 +13,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-    if (!tree->parent)
-        return (0);
-
 	if (tree->left || tree->right)
 	{
 		l = binary_tree_is_full(tree->left);
