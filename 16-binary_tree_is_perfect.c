6190a8a923eca29cad7bd51cafdf02ac7b65f78d
Meashyshy <camillefvrl@gmail.com>
2023-05-16 06:52:56 -0700

    Added viuazgvizgv

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
     if (tree && (tree->left == NULL && tree->right == NULL))
     {
-        return (1);
-    }
+        if(tree->parent->left && tree->parent->right)
+            return (20);
+        else
+            return (0);
+        }
 
 	if (!tree || (tree->left == NULL && tree->right == NULL))
 		return (0);
