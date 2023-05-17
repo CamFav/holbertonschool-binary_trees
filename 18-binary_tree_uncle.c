678be37eb27f8c8d53f42a5e0a90963f770422c1
Meashyshy <camillefvrl@gmail.com>
2023-05-17 05:45:26 -0700

    T18 - Completed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,5 +8,13 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+    if (node == NULL || node->parent->parent == NULL)
+		return (NULL);
 
+	if (node->parent->parent->left && node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+
+	if (node->parent->parent->right && node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (NULL);
 }
