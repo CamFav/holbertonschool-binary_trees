e3382220499f77d063ac4a0eed89125a7644994f
Meashyshy <camillefvrl@gmail.com>
2023-05-17 05:16:07 -0700

    T17 - Completed task

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,12 +8,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left && node->parent->left->n == node->n)
-        return(node->parent->right);
-    if (node->parent->right && node->parent->right->n == node->n)
-        return(node->parent->left);
-    return (NULL);
+	if (node->parent->left && node->parent->right == node)
+		return (node->parent->left);
+
+	if (node->parent->right && node->parent->left == node)
+		return (node->parent->right);
+	return (NULL);
 }
