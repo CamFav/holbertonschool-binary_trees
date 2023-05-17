12a1162b3466d7da1448a122c707d939296610ce
Meashyshy <camillefvrl@gmail.com>
2023-05-17 05:01:24 -0700

    T17 - Completed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
- *  * binary_tree_is_full - function that checks if a binary tree is full.
- *   * @tree: pointer to the root node of the tree to check.
- *    *
- *     * Return: Always 0.
- *      */
+ * binary_tree_is_full - function that checks if a binary tree is full.
+ * @tree: pointer to the root node of the tree to check.
+ * 
+ * Return: Always 0.
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	int l = 0;
@@ -32,11 +32,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 #include "binary_trees.h"
 /**
- *  * binary_tree_height - function that measures the height of a binary tree.
- *   * @tree: pointer to the root node of the tree to meaasure the height.
- *    *
- *     * Return: Always 0.
- *      */
+ * binary_tree_height - function that measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to meaasure the height.
+ *
+ * Return: Always 0.
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_height = 0;
@@ -44,8 +44,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree || (tree->left == NULL && tree->right == NULL))
 		return (0);
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
+
 	if (left_height > right_height)
 		return (left_height + 1);
 	else
@@ -69,7 +71,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int f = 0;
 
 	if (!tree)
-		return (1);
+		return (0);
 	/**if ((!tree->left || !tree->right) && (tree->left && tree->right))*/
 	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 	{
@@ -81,6 +83,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		l += binary_tree_height(tree->left);
 		f += binary_tree_is_full(tree->right);
 		r += binary_tree_height(tree->right);
+
 		if (f == 0)
 		{
 			h = binary_tree_height(tree);
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,5 +8,12 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+    if (node == NULL || node->parent == NULL)
+        return (NULL);
 
+    if (node->parent->left && node->parent->left->n == node->n)
+        return(node->parent->right);
+    if (node->parent->right && node->parent->right->n == node->n)
+        return(node->parent->left);
+    return (NULL);
 }
